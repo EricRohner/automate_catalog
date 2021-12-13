638c5d07f983607b89a3dafc910254562f4732ee
Eric Rohner <ericrohner22@gmail.com>
2021-12-13 16:57:55 -0700

    image resize and jpeg

---
+++ changeImage.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import os, sys
+from PIL import Image
+
+source = os.getcwd() + "/supplier-data/images"
+destination = os.getcwd() + "/supplier-data/images"
+
+def get_source_files():
+        source_files = os.listdir(source)
+        source_files.remove("LICENSE")
+        source_files.remove("README")
+        print(source_files)
+        return source_files
+
+def process_img(file):
+        path = source + "/" + file
+        im = Image.open(path)
+        im = im.resize((600,400))
+        im = im.convert("RGB")
+        save_file = file.replace("tiff","jpeg")
+        im.save(destination + "/" + save_file + ".jpg", format = "jpeg")
+
+if __name__ == "__main__":
+        source_files = get_source_files()
+        for file in source_files:
+                process_img(file)
+
+
