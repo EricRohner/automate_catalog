4eb6ef10ffc2a9c9139ca2d5ab6bdbdfe17e1b12
Eric Rohner <ericrohner22@gmail.com>
2022-02-09 21:11:17 -0600

    run.py

--- run.py
+++ run.py
@@ -2,21 +2,37 @@
 
 import os
 import requests
+import json
+import re
 
 source = os.getcwd() + "/supplier-data/descriptions"
+url = ""
+post_url = "http://{}/fruits".format(url)
 
 def get_files(direc):
-        return os.listdir(direc)
+	return os.listdir(direc)
 
 def process_files(files):
-        json = {}
-        return json
+	for file in files:
+		dict = file_to_dict(file)
+		post_json(dict)
+		print(dict)
 
-def upload_json():
-        #do the thing
-        pass
+def file_to_dict(file):
+	with open(source + "/" + file) as opened:
+		lines = opened.read().splitlines()
+		dict = {"name": lines[0], "weight": remove_char(lines[1]), "description": lines[2].strip(), "image_name": txt_to_jpeg(file)}
+		return json.dumps(dict)
+
+def remove_char(line):
+	return int(re.match(r'\d*', line)[0])
+
+def txt_to_jpeg(file):
+	return re.match(r'(.*)\.txt', file).group(1) + ".jpeg"
+
+def post_json(json):
+	requests.post(post_url, data = json)
 
 if __name__ == "__main__":
-        files = get_files(source)
-        print(files)
-        json = process_files(files)
+	files = get_files(source)
+	process_files(files)
